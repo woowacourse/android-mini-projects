8845f402bed4971eac9194d09e40f52ae34b2d26
todays-sun-day <178suna@gmail.com>
2026-06-04 17:04:52 +0900

    chore: Kotlin Multiplatform 프로젝트 초기 설정 및 기본 구조 생성

---
+++ Dognal/shared/src/iosMain/kotlin/a4/dogsignal/Platform.ios.kt
@@ -0,0 +1,9 @@
+package a4.dogsignal
+
+import platform.UIKit.UIDevice
+
+class IOSPlatform: Platform {
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
+
+actual fun getPlatform(): Platform = IOSPlatform()
