b5e51118e0ef1b62080e4b17003ce8ea1fbecf03
todays-sun-day <178suna@gmail.com>
2026-06-05 16:16:07 +0900

    build: kotlinx-datetime 및 androidx-ui-tooling 라이브러리 의존성 추가

--- Dognal/shared/build.gradle.kts
+++ Dognal/shared/build.gradle.kts
@@ -37,6 +37,7 @@ kotlin {
     sourceSets {
         androidMain.dependencies {
             implementation(libs.compose.uiToolingPreview)
+            implementation(libs.androidx.ui.tooling)
         }
         commonMain.dependencies {
             implementation(libs.compose.runtime)
@@ -47,6 +48,7 @@ kotlin {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.kotlinx.datetime)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
